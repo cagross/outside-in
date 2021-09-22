d3ecd95eddca55ff34e0cd31eea7157bdbcb2a91
Carl Gross <cagross@everlooksolutions.com>
2021-09-22 11:59:56 +0700

    Add error state.

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -30,19 +30,34 @@ describe('RestaurantList', () => {
     beforeEach(() => {
       renderWithProps();
     });
+    it('does not display the loading indicator while not loading', () => {
+      const {queryByTestId} = context;
+      expect(queryByTestId('loading-indicator')).toBeNull();
+    });
+    it('does not display the error message', () => {
+      const {queryByText} = context;
+      expect(queryByText('Restaurants could not be loaded.')).toBeNull();
+    });
     it('displays the restaurants', () => {
       const {queryByText} = context;
       expect(queryByText('Sushi Place')).not.toBeNull();
       expect(queryByText('Pizza Place')).not.toBeNull();
-    });
-    it('does not display the loading indicator while not loading', () => {
-      const {queryByTestId} = context;
-      expect(queryByTestId('loading-indicator')).toBeNull();
     });
   });
   it('displays the loading indicator while loading', () => {
     renderWithProps({loading: true});
     const {queryByTestId} = context;
     expect(queryByTestId('loading-indicator')).not.toBeNull();
+  });
+
+  describe('when loading fails', () => {
+    beforeEach(() => {
+      renderWithProps({loadError: true});
+    });
+
+    it('displays the error message', () => {
+      const {queryByText} = context;
+      expect(queryByText('Restaurants could not be loaded.')).not.toBeNull();
+    });
   });
 });
